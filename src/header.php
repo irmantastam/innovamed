aedc263537fc24f74a40b8cc24f0d388fc8b4051
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2021-05-30 11:26:54 +0300

    Add site favicon

--- src/header.php
+++ src/header.php
@@ -3,14 +3,14 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="google-site-verification" content="f2b5hyRFMnivumtMVMrU_iyjgUJN-MMsN67yxiOkKo4" />
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/safari-pinned-tab.svg" color="#1a353d">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon.ico">
-    <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/favicon/browserconfig.xml">
+    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/innovamed/src/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/innovamed/src/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/innovamed/src/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/wp-content/themes/innovamed/src/assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/wp-content/themes/innovamed/src/assets/favicon/safari-pinned-tab.svg" color="#2d89ef">
+    <link rel="shortcut icon" href="/wp-content/themes/innovamed/src/assets/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="msapplication-config" content="/wp-content/themes/innovamed/src/assets/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
