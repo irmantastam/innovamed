ed9256fd95c89b2d60dfce62233a7658773f6f85
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2021-05-30 11:36:24 +0300

    Remove google site verification tag

--- src/header.php
+++ src/header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="google-site-verification" content="f2b5hyRFMnivumtMVMrU_iyjgUJN-MMsN67yxiOkKo4" />
     <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/innovamed/src/assets/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/innovamed/src/assets/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/innovamed/src/assets/favicon/favicon-16x16.png">
