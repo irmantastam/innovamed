577fb92838e72ee640e2cd5b5a6a42192d1d7276
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2021-08-22 15:42:55 +0300

    Update site favicon

--- src/header.php
+++ src/header.php
@@ -7,9 +7,9 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/innovamed/src/assets/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/innovamed/src/assets/favicon/favicon-16x16.png">
     <link rel="manifest" href="/wp-content/themes/innovamed/src/assets/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/wp-content/themes/innovamed/src/assets/favicon/safari-pinned-tab.svg" color="#2d89ef">
+    <link rel="mask-icon" href="/wp-content/themes/innovamed/src/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="/wp-content/themes/innovamed/src/assets/favicon/favicon.ico">
-    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="msapplication-TileColor" content="#00aba9">
     <meta name="msapplication-config" content="/wp-content/themes/innovamed/src/assets/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
